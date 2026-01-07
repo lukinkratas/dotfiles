14fb2fad4d0c8eaca69c87039ddae003c5cde2a7
Lukin Kratas <lukin.kratas@seznam.cz>
2026-01-07 16:56:22 +0100

    add ghostty rc

--- scripts/link.sh
+++ scripts/link.sh
@@ -6,8 +6,11 @@ for file in .actrc .env .vimrc .zshrc; do
     ln -svf $WORKING_DIR/$file $HOME/
 done
 
+# create .config directory if it doesn't exist and link config files
+[[ ! -d $HOME/.config ]] && mkdir -v $HOME/.config && ln -svf $WORKING_DIR/.config/* $HOME/.config/
+
 # create .claude directory if it doesn't exist and link agents to claude code
-[[ ! -d $HOME/.claude ]] && mkdir -v $HOME/.claude && ln -svf $WORKING_DIR/.claude/agents $HOME/.claude
+[[ ! -d $HOME/.claude ]] && mkdir -v $HOME/.claude && ln -svf $WORKING_DIR/.claude/agents $HOME/.claude/
 
 # link mcps to cluade desktop
 ln -svf $WORKING_DIR/mcp.json "/Users/lukaskratochvila/Library/Application Support/Claude/claude_desktop_config.json"
